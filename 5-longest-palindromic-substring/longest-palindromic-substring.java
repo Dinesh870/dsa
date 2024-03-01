7a08c09fda1c1dbb445ea1c45bedac284127ebf2
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-01 19:21:44 +0530

    Time: 437 ms (20.31%) | Memory: 70.2 MB (5.09%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -7,16 +7,63 @@ class Solution {
         }
         return true;
     }
+    // String fun(int i, int j, String s, String[] ans) {
+
+    //     if(i>j) return "";
+        
+    //     if(isPalindrome(s, i, j)) {
+    //         // if((j-i) >= ans[0].length()) 
+    //         //     ans[0] = s.substring(i,j+1);
+    //         return s.substring(i,j+1);
+    //     }
+
+    //     String left = fun(i+1,j,s,ans);
+    //     String right = fun(i,j-1,s, ans);
+
+    //     if(left.length() > right.length()) return left;
+
+    //     return right;
+    // }
+    String fun(int i, int j, String s, String[][] dp) {
+
+        if(i>j) return "";
+        if(dp[i][j] != null) return dp[i][j];
+        
+        if(isPalindrome(s, i, j)) {
+            // if((j-i) >= ans[0].length()) 
+            //     ans[0] = s.substring(i,j+1);
+            return dp[i][j] = s.substring(i,j+1);
+        }
+
+        String left = fun(i+1,j,s, dp);
+        String right = fun(i,j-1,s, dp);
+
+        if(left.length() > right.length()) return dp[i][j] = left;
+
+        return dp[i][j] = right;
+    }
     public String longestPalindrome(String s) {
-        String temp = "";
+        // String temp = "";
         int n = s.length();
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(isPalindrome(s, i, j) && ((j-i) >= temp.length())) {
-                    temp = s.substring(i,j+1);
-                }
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = i; j < n; j++) {
+        //         if(isPalindrome(s, i, j) && ((j-i) >= temp.length())) {
+        //             temp = s.substring(i,j+1);
+        //         }
+        //     }
+        // }
+        // return temp;
+        // String[] ans = new String[1];
+        // ans[0] = "";
+        // fun(0, n-1, s, ans);
+        // return ans[0];
+
+        String[][] dp = new String[n][n];
+        for(int i = 0; i < n ;i++) {
+            for(int j = 0; j < n; j++) {
+                dp[i][j] = null;
             }
         }
-        return temp;
+        return fun(0, n-1, s, dp);
     }
 }
