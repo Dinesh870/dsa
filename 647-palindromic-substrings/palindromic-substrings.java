389eced43d9a9b34353f3b8393eb90a7c1295a16
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-01 16:10:59 +0530

    Time: 731 ms (5.04%) | Memory: 41.4 MB (76.52%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -7,14 +7,32 @@ class Solution {
         }
         return true;
     }
-    public int countSubstrings(String s) {
-        int cnt = 0;
-        int n = s.length();
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(isPalindrome(s, i, j)) cnt++;
-            }
+    void fun(int i, String s, int[] ans) {
+
+        if(i < 0) return;
+
+        for(int j = i; j < s.length(); j++) {
+            if(isPalindrome(s,i,j)) 
+                ans[0]++;
         }
-        return cnt;
+        fun(i-1, s,ans);
+    } 
+    public int countSubstrings(String s) {
+        // Brute force
+        // int cnt = 0;
+        int n = s.length();
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = i; j < n; j++) {
+        //         if(isPalindrome(s, i, j)) cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        // dp
+        // memoization
+        int[] ans = new int[1];
+        fun(n-1, s, ans);
+
+        return ans[0];
     }
 }
