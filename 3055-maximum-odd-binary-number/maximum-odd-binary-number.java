c817dc02de085a83997b26a539ef29061bc7a5e1
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-01 22:58:40 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (58.27%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.java
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.java
@@ -1,17 +1,29 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
+        // int one = 0;
+        // int n = s.length();
+
+        // for(int i = 0; i < n; i++) {
+        //     if(s.charAt(i) == '1') one++;
+        // }
+        // int zero = n - one;
+        // s = "";
+        // for(int i = 0; i < one-1; i++) s = s + "1";
+        // for(int i = 0; i < zero; i++) s = s + "0";
+        // System.out.println(s);
+        // s = s + "1";
+        // return s;
+
+        // method 2:
+        char[] ch = s.toCharArray();
         int one = 0;
         int n = s.length();
-
         for(int i = 0; i < n; i++) {
-            if(s.charAt(i) == '1') one++;
+            if(ch[i] == '1') one++;
         }
-        int zero = n - one;
-        s = "";
-        for(int i = 0; i < one-1; i++) s = s + "1";
-        for(int i = 0; i < zero; i++) s = s + "0";
-        System.out.println(s);
-        s = s + "1";
-        return s;
+        for(int i = 0; i < one-1; i++) ch[i] = '1';
+        for(int i = one-1; i < n; i++) ch[i] = '0';
+        ch[n-1] = '1';
+        return new String(ch);
     }
 }
