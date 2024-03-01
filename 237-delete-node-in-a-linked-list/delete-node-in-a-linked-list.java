743ce0eec19b77472c0889e8c6f249ba25083673
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-02 02:06:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (21.82%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -8,11 +8,11 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        while(node.next != null && node.next.next != null) {
-            node.val = node.next.val;
-            node = node.next;
-        }
+        // while(node.next != null && node.next.next != null) {
+        //     node.val = node.next.val;
+        //     node = node.next;
+        // }
         node.val = node.next.val;
-        node.next = null;
+        node.next = node.next.next;
     }
 }
