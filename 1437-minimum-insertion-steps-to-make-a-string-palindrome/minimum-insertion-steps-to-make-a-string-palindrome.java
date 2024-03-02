61f765a90fe2ac4a92da03ae1edef057a30493b5
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-03 02:10:50 +0530

    Time: 19 ms (82.45%) | Memory: 45.3 MB (59.35%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -5,10 +5,11 @@ class Solution {
         if(s.charAt(i) == s.charAt(j)) {
             return dp[i][j] = 0 + fun(s, i + 1, j-1, dp);
         }
-        return dp[i][j] = Math.min(1 + fun(s, i+1, j, dp) , 1 + fun(s, i, j-1, dp));
+        return dp[i][j] = 1 + Math.min(fun(s, i+1, j, dp) , fun(s, i, j-1, dp));
     }
     public int minInsertions(String s) {
         int n = s.length();
+        // memoization
         int[][] dp = new int[n][n];
         for(int[] arr : dp) Arrays.fill(arr, -1);
 
@@ -20,5 +21,17 @@ class Solution {
         //     System.out.println();
         // }
         return ans;
+
+        // tabulation
+        // Base case
+
+        // for(int i = 1; i < n; i++) {
+        //     for(int j = 1; j < n; j++) {
+        //         if(s.charAt(i) == s.charAt(j))
+        //             dp[i][j] = 0 + dp[i+1][j-1];
+        //         else
+        //             dp[i][j] = Math.min(1 + fun(s, i+1, j, dp) , 1 + fun(s, i, j-1, dp));
+        //     }
+        // }
     }
 }
