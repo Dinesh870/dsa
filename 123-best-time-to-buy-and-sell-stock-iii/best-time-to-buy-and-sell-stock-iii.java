1df3aae3a6d140fe2c488f62aac938f7274fa641
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-03 15:48:54 +0530

    Time: 70 ms (32.07%) | Memory: 63.6 MB (32.79%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -31,24 +31,47 @@ class Solution {
 
         // tabulation
 
+        // for(int i = n-1; i >= 0; i--) {
+        //     for(int buy = 0; buy <= 1; buy++) {
+        //         for(int cap = 1; cap <= 3; cap++) {
+        //             int profit = 0;
+
+        //             if(buy==1) {
+        //                 int canbuy = - prices[i] + dp[i+1][0][cap];
+        //                 int notbuy = 0 + dp[i+1][1][cap];
+        //                 profit = Math.max(canbuy, notbuy);
+        //             } else {
+        //                 int cansell = prices[i] + dp[i+1][1][cap-1];
+        //                 int notsell = 0 + dp[i+1][0][cap];
+        //                 profit = Math.max(cansell, notsell);
+        //             }
+        //             dp[i][buy][cap] = profit;
+        //         }
+        //     }
+        // }
+        // return dp[0][1][2];
+
+        // space optimization
+        int[][] prev = new int[2][4];
+
         for(int i = n-1; i >= 0; i--) {
             for(int buy = 0; buy <= 1; buy++) {
                 for(int cap = 1; cap <= 3; cap++) {
                     int profit = 0;
 
                     if(buy==1) {
-                        int canbuy = - prices[i] + dp[i+1][0][cap];
-                        int notbuy = 0 + dp[i+1][1][cap];
+                        int canbuy = - prices[i] + prev[0][cap];
+                        int notbuy = 0 + prev[1][cap];
                         profit = Math.max(canbuy, notbuy);
                     } else {
-                        int cansell = prices[i] + dp[i+1][1][cap-1];
-                        int notsell = 0 + dp[i+1][0][cap];
+                        int cansell = prices[i] + prev[1][cap-1];
+                        int notsell = 0 + prev[0][cap];
                         profit = Math.max(cansell, notsell);
                     }
-                    dp[i][buy][cap] = profit;
+                    prev[buy][cap] = profit;
                 }
             }
         }
-        return dp[0][1][2];
+        return prev[1][2];
     }
 }
