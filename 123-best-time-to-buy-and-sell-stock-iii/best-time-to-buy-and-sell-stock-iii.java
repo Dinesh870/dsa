e7503ab1d9b243e6ce1a7470e463ab239620bb31
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-03 15:31:10 +0530

    Time: 86 ms (24.90%) | Memory: 64.2 MB (26.96%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -16,17 +16,39 @@ class Solution {
             int notsell = 0 + fun(i+1, 0, cap, prices, dp);
             profit = Math.max(cansell, notsell);
         }
-        // System.out.println(profit);
         return dp[i][buy][cap] = profit;
     }
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for(int[][] arr : dp) {
-            for(int[] arr2 : arr) {
-                Arrays.fill(arr2, -1);
+        // memoization
+        int[][][] dp = new int[n+1][2][4];
+        // for(int[][] arr : dp) {
+        //     for(int[] arr2 : arr) {
+        //         Arrays.fill(arr2, -1);
+        //     }
+        // }
+        // return fun(0, 1, 2, prices, dp);
+
+        // tabulation
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                for(int cap = 1; cap <= 3; cap++) {
+                    int profit = 0;
+
+                    if(buy==1) {
+                        int canbuy = - prices[i] + dp[i+1][0][cap];
+                        int notbuy = 0 + dp[i+1][1][cap];
+                        profit = Math.max(canbuy, notbuy);
+                    } else {
+                        int cansell = prices[i] + dp[i+1][1][cap-1];
+                        int notsell = 0 + dp[i+1][0][cap];
+                        profit = Math.max(cansell, notsell);
+                    }
+                    dp[i][buy][cap] = profit;
+                }
             }
         }
-        return fun(0, 1, 2, prices, dp);
+        return dp[0][1][2];
     }
 }
