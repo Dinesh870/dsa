fbd79c6eb01ebd8f97c20bf95f9c4a354722fa0a
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-03 14:23:57 +0530

    Time: 1 ms (93.56%) | Memory: 45.8 MB (35.24%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -24,43 +24,43 @@ class Solution {
 
         // tabulation
 
-        for(int i = n-1; i >= 0; i--) {
-            for(int buy = 0; buy <= 1; buy++) {
-                int profit = 0;
-                if(buy == 1) {
-                    int canbuy = - prices[i] + dp[i+1][0];
-                    int notbuy = 0 + dp[i+1][1];
-                    profit = Math.max(canbuy, notbuy);
-                } else {
-                    int cansell = prices[i] + dp[i+1][1];
-                    int notsell = 0 + dp[i+1][0];
-                    profit = Math.max(cansell, notsell);
-                }
-                dp[i][buy] = profit;
-            }
-        }
-        
-        return dp[0][1];
-
-        // space optimization
-        // int[] curr = ne int[2];
         // for(int i = n-1; i >= 0; i--) {
         //     for(int buy = 0; buy <= 1; buy++) {
         //         int profit = 0;
         //         if(buy == 1) {
-        //             int canbuy = - prices[i] + curr[0];
-        //             int notbuy = 0 + curr[1];
+        //             int canbuy = - prices[i] + dp[i+1][0];
+        //             int notbuy = 0 + dp[i+1][1];
         //             profit = Math.max(canbuy, notbuy);
         //         } else {
-        //             int cansell = prices[i] + curr[1];
-        //             int notsell = 0 + curr[0];
+        //             int cansell = prices[i] + dp[i+1][1];
+        //             int notsell = 0 + dp[i+1][0];
         //             profit = Math.max(cansell, notsell);
         //         }
-        //         curr[buy] = profit;
+        //         dp[i][buy] = profit;
         //     }
         // }
+        
+        // return dp[0][1];
 
-        // return curr[1];
+        // space optimization
+        int[] curr = new int[2];
+        for(int i = n-1; i >= 0; i--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if(buy == 1) {
+                    int canbuy = - prices[i] + curr[0];
+                    int notbuy = 0 + curr[1];
+                    profit = Math.max(canbuy, notbuy);
+                } else {
+                    int cansell = prices[i] + curr[1];
+                    int notsell = 0 + curr[0];
+                    profit = Math.max(cansell, notsell);
+                }
+                curr[buy] = profit;
+            }
+        }
+
+        return curr[1];
 
     }
 }
