5f1c717de7d83e97ac602f885d19e530babcd2d3
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-04 08:32:54 +0530

    Time: 3 ms (37.26%) | Memory: 42.6 MB (73.25%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int n = tokens.length;
+        if(n == 0 || tokens[0] > power) return 0;
+        int i = 0, j = n-1;
+        int score = 0;
+        int ans = 0;
+        int totalSum = 0;
+        for(int ints : tokens) totalSum += ints;
+
+        while(i <= j) {
+            if(power >= tokens[i]) {
+                power -= tokens[i];
+                i++;
+                score += 1;
+                ans = Math.max(ans, score);
+            } else if (score > 0) {
+                power += tokens[j];
+                j--;
+                score--;
+            } else break;
+            
+        }
+        return ans;
+    }
+}
