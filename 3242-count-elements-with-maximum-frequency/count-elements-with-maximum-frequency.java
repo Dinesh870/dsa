222fb45e8f6748aeab18b8594b2b8c6caae635be
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-08 09:47:39 +0530

    Time: 1 ms (99.64%) | Memory: 41.7 MB (98.78%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -5,15 +5,26 @@ class Solution {
         for(int i : nums) {
             count[i]++;
         }
-        Arrays.sort(count);
-        if(count[100] == 1) return n;
+        // Arrays.sort(count);
+        // if(count[100] == 1) return n;
         int ans = 0;
-        int i = 100;
-        while(count[i] == count[i-1]) {
-            ans += count[i];
-            i--;
-        }
+        // int i = 100;
+        // while(count[i] == count[i-1]) {
+        //     ans += count[i];
+        //     i--;
+        // }
         
-        return ans+count[i];
+        // return ans+count[i];
+
+        // method 2:
+        int maxi = 0;
+        for(int i = 0; i <= 100; i++) {
+            maxi = Math.max(maxi, count[i]);
+        }
+        if(maxi == 1) return n;
+        for(int i = 0; i <= 100; i++) {
+            if(count[i] == maxi) ans += count[i];
+        }
+        return ans;
     }
 }
