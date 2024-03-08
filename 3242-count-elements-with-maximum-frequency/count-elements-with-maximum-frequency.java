f84325361a2950e6d9a7bb1d81411cf78319680d
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-08 09:42:16 +0530

    Time: 2 ms (70.74%) | Memory: 41.6 MB (98.78%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -8,16 +8,12 @@ class Solution {
         Arrays.sort(count);
         if(count[100] == 1) return n;
         int ans = 0;
-        boolean flag = false;
         int i = 100;
         while(count[i] == count[i-1]) {
             ans += count[i];
-            flag = true;
             i--;
         }
-        // if(ans == 0) {
-        //     return count[100];
-        // }
+        
         return ans+count[i];
     }
 }
