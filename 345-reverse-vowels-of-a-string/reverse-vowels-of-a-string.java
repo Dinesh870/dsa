7d698d7d47f61ef0179df4d758360c221aa9ac9b
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-09 12:08:31 +0530

    Time: 99 ms (11.67%) | Memory: 46.2 MB (10.46%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,28 +1,48 @@
 class Solution {
     public String reverseVowels(String s) {
         int p = 0, q = s.length()-1;
-        char[] charArr = s.toCharArray();
-        HashSet<Character> set = new HashSet<>();
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
-        set.add('A');
-        set.add('E');
-        set.add('I');
-        set.add('O');
-        set.add('U');
+        // char[] charArr = s.toCharArray();
+        // HashSet<Character> set = new HashSet<>();
+        // set.add('a');
+        // set.add('e');
+        // set.add('i');
+        // set.add('o');
+        // set.add('u');
+        // set.add('A');
+        // set.add('E');
+        // set.add('I');
+        // set.add('O');
+        // set.add('U');
+        // while(p < q) {
+        //     if(set.contains(charArr[p]) && set.contains(charArr[q])) {
+        //         char t = charArr[p];
+        //         charArr[p] = charArr[q];
+        //         charArr[q] = t;
+        //         p++;
+        //         q--;
+        //     }else if(!set.contains(charArr[p])) p++;
+        //     else q--;
+        // }
+        // return new String(charArr);
+
+        // Method 2:
+        StringBuilder sb = new StringBuilder(s);
+        String vowels = "aeiouAEIOU";
+
         while(p < q) {
-            if(set.contains(charArr[p]) && set.contains(charArr[q])) {
-                char t = charArr[p];
-                charArr[p] = charArr[q];
-                charArr[q] = t;
+            char chs = sb.charAt(p);
+            char chl = sb.charAt(q);
+            
+            if(vowels.contains(chs+"") && vowels.contains(chl+"")) {
+                sb.deleteCharAt(q);
+                sb.deleteCharAt(p);
+                sb.insert(p, chl);
+                sb.insert(q, chs);
                 p++;
                 q--;
-            }else if(!set.contains(charArr[p])) p++;
-            else q--;
+            }else if(vowels.contains(chs+"")) q--;
+            else p++;
         }
-        return new String(charArr);
+        return sb.toString();
     }
 }
