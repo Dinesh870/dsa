bc2661431fb339e21ec9e8ab327e289752013ba2
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-10 10:54:17 +0530

    Time: 4 ms (32.21%) | Memory: 42.7 MB (93.83%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,4 +1,14 @@
 class Solution {
+    boolean search(int[] arr, int k) {
+        int i = 0, j= arr.length-1;
+        while(i <= j) {
+            int mid = (i+j)/2;
+            if(arr[mid] == k) return true;
+            else if(k > arr[mid]) i = mid+1;
+            else j = mid-1;
+        }
+        return false;
+    }
     public int[] intersection(int[] nums1, int[] nums2) {
         // Set<Integer> set1 = new HashSet<>();
         // Set<Integer> set2 = new HashSet<>();
@@ -17,24 +27,40 @@ class Solution {
 
         // method 2:
         
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+        // Set<Integer> set = new HashSet<>();
+
+        // int i = 0, j = 0;
+        // while(i < nums1.length && j < nums2.length) {
+        //     if(nums1[i] == nums2[j]) {
+        //         set.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }else if(nums1[i] > nums2[j]) j++;
+        //     else i++;
+        // }        
+        // int[] ans = new int[set.size()];
+        // int k = 0;
+        // for(var v : set) {
+        //     ans[k++] = v;
+        // }
+        // return ans;
+
+        // method 3:
+
         Arrays.sort(nums1);
-        Arrays.sort(nums2);
         Set<Integer> set = new HashSet<>();
 
-        int i = 0, j = 0;
-        while(i < nums1.length && j < nums2.length) {
-            if(nums1[i] == nums2[j]) {
-                set.add(nums1[i]);
-                i++;
-                j++;
-            }else if(nums1[i] > nums2[j]) j++;
-            else i++;
-        }        
+        for(int i = 0; i < nums2.length; i++) {
+            if(search(nums1, nums2[i])) set.add(nums2[i]);
+        }
         int[] ans = new int[set.size()];
         int k = 0;
         for(var v : set) {
             ans[k++] = v;
         }
         return ans;
+        
     }
 }
