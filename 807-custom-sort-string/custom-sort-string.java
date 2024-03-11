a8386862c62bdd13f84178983249e5915030ef65
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-11 09:17:43 +0530

    Time: 1 ms (75.87%) | Memory: 40.7 MB (99.52%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,36 +1,63 @@
 class Solution {
     public String customSortString(String order, String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        // count no of character
+        // Map<Character,Integer> map = new HashMap<>();
+        // // count no of character
+        // for(int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     if(!map.containsKey(ch)) map.put(ch,1);
+        //     else {
+        //         int x = map.get(ch);
+        //         map.put(ch, x+1);
+        //     }
+        // }
+
+        // // sorting
+        // s = "";
+        // for(int i = 0; i < order.length(); i++) {
+        //     char ch = order.charAt(i);
+        //     if(!map.containsKey(ch)) continue;
+        //     int f = map.get(ch);
+        //     while(f > 0) {
+        //         s += ch;
+        //         f--;
+        //     }
+        //     map.remove(ch);
+        // }
+        // for(var ch : map.keySet()){
+        //     int x = map.get(ch);
+        //     while(x > 0) {
+        //         s += ch;
+        //         x--;
+        //     }
+        // }
+
+        // return s;
+
+        // method 2:
+        int[] store = new int[26];
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(!map.containsKey(ch)) map.put(ch,1);
-            else {
-                int x = map.get(ch);
-                map.put(ch, x+1);
-            }
+            store[ch-'a']++;
         }
-
-        // sorting
-        s = "";
+        // for(int i : store) {
+        //     System.out.print(i+" ");
+        // }
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < order.length(); i++) {
             char ch = order.charAt(i);
-            if(!map.containsKey(ch)) continue;
-            int f = map.get(ch);
-            while(f > 0) {
-                s += ch;
-                f--;
-            }
-            map.remove(ch);
-        }
-        for(var ch : map.keySet()){
-            int x = map.get(ch);
-            while(x > 0) {
-                s += ch;
-                x--;
+            // int x = store[ch-'a'];
+            while(store[ch-'a'] > 0) {
+                sb.append(ch);
+                store[ch-'a']--;
             }
         }
-
-        return s;
+        for(int i = 0; i < 26; i++) {
+            while(store[i] > 0) {
+                char ch = (char)(97 + i);
+                sb.append(ch);
+                store[i]--;
+            }
+        }
+        return sb.toString();
     }
 }
