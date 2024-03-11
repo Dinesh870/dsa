d2567b754ffbf7e06743631067b0bfceb3003ca0
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-11 10:02:51 +0530

    Time: 2 ms (94.81%) | Memory: 52.2 MB (61.06%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.java
@@ -1,19 +1,21 @@
 class Solution {
-    int search(int[][] score, int target, int k) {
-        for(int i = 0; i < score.length; i++) {
-            if(score[i][k] == target) return i;
-        }
-        return -1;
-    }
-    void swap(int[][] score, int i, int j) {
-        int col = score[0].length;
-        for(int k = 0; k < col; k++) {
-            int temp = score[i][k];
-            score[i][k] = score[j][k];
-            score[j][k] = temp;
-        }
-    }
+    // int search(int[][] score, int target, int k) {
+    //     for(int i = 0; i < score.length; i++) {
+    //         if(score[i][k] == target) return i;
+    //     }
+    //     return -1;
+    // }
+    // void swap(int[][] score, int i, int j) {
+    //     int col = score[0].length;
+    //     for(int k = 0; k < col; k++) {
+    //         int temp = score[i][k];
+    //         score[i][k] = score[j][k];
+    //         score[j][k] = temp;
+    //     }
+    // }
     public int[][] sortTheStudents(int[][] score, int k) {
+        // method 1:
+
         // int m = score.length;
         // int n = score[0].length;
         // int[] temp = new int[m];
@@ -31,6 +33,7 @@ class Solution {
         // }
         // return score;
 
+        // method 2:
         Arrays.sort(score, (a,b) -> b[k] - a[k]);
         return score;
     }
