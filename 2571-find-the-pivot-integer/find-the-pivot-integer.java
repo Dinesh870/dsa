67c49f14e930b06e5badc7be7ebded370deb42f9
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-13 19:52:32 +0530

    Time: 1 ms (77.54%) | Memory: 40.4 MB (78.99%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,15 +1,28 @@
 class Solution {
     public int pivotInteger(int n) {
-        int[] pref = new int[n+1];
-        for(int i = 1; i<= n; i++) {
-            pref[i] = pref[i-1] + i;
-        }
+        // int[] pref = new int[n+1];
+        // for(int i = 1; i<= n; i++) {
+        //     pref[i] = pref[i-1] + i;
+        // }
+        // int sum = 0;
+        // for(int i = n; i > 0; i--) {
+        //     sum += i;
+        //     if(pref[i] == sum) return i;
+        // }
+
+        // return -1;
+
+        // method 2:
+        int prefSum = 0;
+        for(int i = 1; i <= n; i++) prefSum += i;
         int sum = 0;
         for(int i = n; i > 0; i--) {
             sum += i;
-            if(pref[i] == sum) return i;
+            if(sum == prefSum) return i;
+            else {
+                prefSum -= i;
+            }
         }
-
         return -1;
     }
 }
