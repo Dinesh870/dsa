7eea7b70b1bac3e2eada13113edacf44d5dd1624
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-17 19:57:55 +0530

    Time: 307 ms (5.14%) | Memory: 60.3 MB (5.33%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -18,23 +18,52 @@ class Solution {
         // }
         // return cnt;
 
-        // method 2:
-        Arrays.sort(nums);
-        int p = 0, q = n-1;
-        while(p < q) {
-            if(nums[p] == -1) p++;
-            if(nums[q] == -1) q--;
+        // method 2: T.C. = O(nlog(n) + n)
+        // Arrays.sort(nums);
+        // int p = 0, q = n-1;
 
-            int sum = nums[p] + nums[q];
-            if(sum == k) {
-                nums[p] = -1;
-                nums[q] = -1;
-                cnt++;
-                p++;
-                q--;
+        // while(p < q) {
+        //     if(nums[p] == -1) p++;
+        //     if(nums[q] == -1) q--;
+
+        //     int sum = nums[p] + nums[q];
+
+        //     if(sum == k) {
+        //         nums[p] = -1;
+        //         nums[q] = -1;
+        //         cnt++;
+        //         p++;
+        //         q--;
+        //     }
+
+        //     if(sum > k) q--;
+        //     if(sum < k) p++;
+        // }
+        // return cnt;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            if(!map.containsKey(nums[i])) {
+                map.put(nums[i], 1);
+            }else {
+                int x = map.get(nums[i]);
+                map.put(nums[i], x+1);
             }
-            if(sum > k) q--;
-            if(sum < k) p++;
+        }
+        for(int i = 0; i < n; i++) {
+            int key = k - nums[i];
+            if(!map.containsKey(key)) continue;
+
+            int x = map.get(key);
+            int y = map.get(nums[i]);
+            if(key == nums[i] && map.get(key) == 1) continue;
+            if (x > 0 && y > 0) {
+                System.out.print(x+" "+y+",");
+                cnt++;
+                map.put(key,map.get(key)-1);
+                map.put(nums[i],map.get(nums[i])-1);
+            }
         }
         return cnt;
     }
