d1ce627fc3cb6644e53d71e118885af533ab78e4
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-17 20:05:25 +0530

    Time: 54 ms (6.07%) | Memory: 57.6 MB (29.10%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -3,44 +3,9 @@ class Solution {
         int n = nums.length;
         int cnt = 0;
 
-        // method 1: Brute force, T.C. = O(n^2)
-        // for(int i = 0; i < n; i++) {
-        //     if(nums[i] == -1) continue;
-        //     for(int j = i+1; j < n; j++) {
-        //         if(nums[j] == -1) continue;
-        //         if(nums[i] + nums[j] == k) {
-        //             cnt++;
-        //             nums[i] = -1;
-        //             nums[j] = -1;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return cnt;
 
-        // method 2: T.C. = O(nlog(n) + n)
-        // Arrays.sort(nums);
-        // int p = 0, q = n-1;
 
-        // while(p < q) {
-        //     if(nums[p] == -1) p++;
-        //     if(nums[q] == -1) q--;
-
-        //     int sum = nums[p] + nums[q];
-
-        //     if(sum == k) {
-        //         nums[p] = -1;
-        //         nums[q] = -1;
-        //         cnt++;
-        //         p++;
-        //         q--;
-        //     }
-
-        //     if(sum > k) q--;
-        //     if(sum < k) p++;
-        // }
-        // return cnt;
-
+        // method 3: T.C. = O(n), S.C. = O(n)
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < n; i++) {
@@ -51,12 +16,13 @@ class Solution {
                 map.put(nums[i], x+1);
             }
         }
+
         for(int i = 0; i < n; i++) {
             int key = k - nums[i];
             if(!map.containsKey(key)) continue;
 
             if(key == nums[i] && map.get(key) == 1) continue;
-            
+
             if (map.get(key) > 0 && map.get(nums[i]) > 0) {
                 cnt++;
                 map.put(key,map.get(key)-1);
