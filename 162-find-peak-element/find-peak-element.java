35041d08a0cacdea78d22dc44a0cd4b264c8b687
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-20 10:45:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (95.11%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -3,10 +3,10 @@ class Solution {
         // method 1: Brute force: T.C. = O(n) , S.C. = O(1)
         int n = nums.length;
         if(n == 1) return 0;
-        if(n == 2) {
+        // if(n == 2) {
             if(nums[0] > nums[1]) return 0;
-            else return 1;
-        }
+            if(nums[n-1] > nums[n-2]) return n-1;
+        // }
         //     if(nums[0] > nums[1]) return 0;
         // for(int i = 1; i < n-1; i++) {
         //     if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
@@ -16,16 +16,16 @@ class Solution {
 
         // method 2: Binary search
 
-        int st = 0, end = n-1;
+        int st = 1, end = n-2;
 
         while(st <= end) {
             int mid = (st+end)/2;
-            if(mid==n-1) {
-                if(nums[mid] > nums[mid-1]) return mid;
-            }
-            if(mid == 0) {
-                if(nums[mid] > nums[mid+1]) return mid;
-            }
+            // if(mid==n-1) {
+            //     if(nums[mid] > nums[mid-1]) return mid;
+            // }
+            // if(mid == 0) {
+            //     if(nums[mid] > nums[mid+1]) return mid;
+            // }
             if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) return mid;
             if(nums[mid] < nums[mid+1]) st = mid+1;
             else end = mid-1;
