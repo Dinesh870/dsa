de8a340aae394a914ec39f904be7202e3be479ef
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-22 17:27:19 +0530

    Time: 10 ms (91.88%) | Memory: 47 MB (68.52%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,73 @@
+class Solution {
+
+    // method 1:
+    int fun(int[] weights, int cnt) {
+        int minCapacity = 0, maxCapacity = 0;
+        for(int i : weights) {
+            minCapacity = Math.max(minCapacity, i);
+            maxCapacity += i;
+        }
+
+       while(minCapacity <= maxCapacity) {
+             int sum = 0;
+             int days = cnt;
+
+            for(int i : weights) {
+                
+                if(sum + i > minCapacity) {
+                    days--;
+                    sum = i;
+                } else {
+                    sum += i;
+                }
+
+            }
+            System.out.print(days+" ");
+            if(days > 0) return minCapacity;
+            minCapacity++;
+
+       }
+       return 0;
+    }
+
+    // method 2: Binary search
+    int binarySearch(int[] weights, int cnt) {
+
+        int minCapacity = 0, maxCapacity = 0;
+        for(int i : weights) {
+            minCapacity = Math.max(minCapacity, i);
+            maxCapacity += i;
+        }
+
+        int ans = 0;
+        while(minCapacity <= maxCapacity) {
+            int capacity = minCapacity + (maxCapacity-minCapacity)/2;
+            int sum = 0, days = cnt;
+
+            for(int i : weights) {
+                
+                if(sum + i > capacity) {
+                    days--;
+                    sum = i;
+                } else {
+                    sum += i;
+                }
+            }
+
+            if(days > 0) {
+                ans = capacity;
+                maxCapacity = capacity-1;
+            }else minCapacity = capacity+1;
+        }
+        return ans;
+
+    }
+
+    public int shipWithinDays(int[] weights, int days) {
+        int n = weights.length;
+        // return fun(weights, days);
+
+        return binarySearch(weights, days);
+        
+    }
+}
