6faf1e92109f20634e928d9a0d59617a40dc8289
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-03-26 17:50:52 +0530

    Time: 21 ms (14.03%) | Memory: 45.9 MB (77.03%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -3,25 +3,41 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
 
-        for(int i = 0; i < n; i++) {
-            int flag = 0;
-            for(int j = i+1; j < n; j++) {
-                if(nums[i] < nums[j]) {
-                    flag = 1;
-                    ans[i] = nums[j];
-                    break;
+        // method 1: T.C. = O(n*n), S.C. = O(1)
+        // for(int i = 0; i < n; i++) {
+        //     int flag = 0;
+        //     for(int j = i+1; j < n; j++) {
+        //         if(nums[i] < nums[j]) {
+        //             flag = 1;
+        //             ans[i] = nums[j];
+        //             break;
+        //         }
+        //     }
+        //     if(flag == 0) {
+        //         for(int j = 0; j < i; j++) {
+        //             if(nums[i] < nums[j]) {
+        //                 flag = 1;
+        //                 ans[i] = nums[j];
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     if(flag == 0) ans[i] = -1;
+        // }
+        // return ans;
+
+        // method 2: using stack
+        Stack<Integer> st = new Stack<>();
+        int k = n-1;
+        for(int i = 2*n-1; i >= 0; i--) {
+            if(st.isEmpty()) st.push(i);
+            else {
+                while(!st.isEmpty() && nums[st.peek()%n] <= nums[i%n]) {
+                    st.pop();
                 }
+                ans[i%n] = st.size() > 0 ? nums[st.peek()%n] : -1;
+                st.push(i);
             }
-            if(flag == 0) {
-                for(int j = 0; j < i; j++) {
-                    if(nums[i] < nums[j]) {
-                        flag = 1;
-                        ans[i] = nums[j];
-                        break;
-                    }
-                }
-            }
-            if(flag == 0) ans[i] = -1;
         }
         return ans;
     }
