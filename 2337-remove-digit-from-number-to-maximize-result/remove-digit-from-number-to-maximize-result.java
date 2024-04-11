8f4883c3f530b9f4bb3530f83a37a6d768298aa8
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-04-11 08:31:36 +0530

    Time: 2 ms (52.26%) | Memory: 42.1 MB (30.89%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
@@ -2,19 +2,25 @@ class Solution {
     public String removeDigit(String number, char digit) {
         int n = number.length();
         ArrayList<Integer> list = new ArrayList<>();
+        // StringBuilder st = new StringBuilder(number);
+        String ans = "";
 
         for(int i = 0; i < n; i++) {
-            if(number.charAt(i) == digit) list.add(i);
-        }
-        // if(list.size() == 1) return number.substring(0,list.get(0)) + number.substring(list.get(0)+1,n);
-        StringBuilder st = new StringBuilder(number);
-        String ans = "";
-        for(int i = 0; i < list.size(); i++) {
-            String temp = number.substring(0,list.get(i)) + number.substring(list.get(i)+1,n);
-            if(temp.compareTo(ans) >= 0) {
-                ans = temp;
+            if(number.charAt(i) == digit) {
+                list.add(i);
+                String temp = number.substring(0,i) + number.substring(i+1,n);
+                if(temp.compareTo(ans) >= 0) {
+                    ans = temp;
+                }
             }
         }
+        
+        // for(int i = 0; i < list.size(); i++) {
+        //     String temp = number.substring(0,list.get(i)) + number.substring(list.get(i)+1,n);
+        //     if(temp.compareTo(ans) >= 0) {
+        //         ans = temp;
+        //     }
+        // }
         return ans;
     }
 }
