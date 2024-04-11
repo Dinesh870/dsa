7df8b78eb46fce246e0c8f28f9792a0a836b1fa1
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-04-11 08:32:14 +0530

    Time: 2 ms (52.26%) | Memory: 42.1 MB (30.89%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.java
@@ -1,13 +1,10 @@
 class Solution {
     public String removeDigit(String number, char digit) {
         int n = number.length();
-        ArrayList<Integer> list = new ArrayList<>();
-        // StringBuilder st = new StringBuilder(number);
         String ans = "";
 
         for(int i = 0; i < n; i++) {
             if(number.charAt(i) == digit) {
-                list.add(i);
                 String temp = number.substring(0,i) + number.substring(i+1,n);
                 if(temp.compareTo(ans) >= 0) {
                     ans = temp;
@@ -15,12 +12,6 @@ class Solution {
             }
         }
         
-        // for(int i = 0; i < list.size(); i++) {
-        //     String temp = number.substring(0,list.get(i)) + number.substring(list.get(i)+1,n);
-        //     if(temp.compareTo(ans) >= 0) {
-        //         ans = temp;
-        //     }
-        // }
         return ans;
     }
 }
