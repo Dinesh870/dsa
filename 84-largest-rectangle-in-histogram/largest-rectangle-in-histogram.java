484cb832e2d1dac9070a7120ba1e5c2a605a18e0
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-04-13 16:55:46 +0530

    Time: 116 ms (18.29%) | Memory: 57.3 MB (59.93%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -4,14 +4,14 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         for(int i = 0; i < n; i++) {
-            if(st.isEmpty()) {
-                st.push(i);
-                ans[i] = 0;
-            } else {
+            // if(st.isEmpty()) {
+            //     st.push(i);
+            //     ans[i] = 0;
+            // } else {
                 while(!st.isEmpty() && nums[st.peek()] >= nums[i]) st.pop();
                 ans[i] = st.size() > 0 ? st.peek()+1 : 0;
                 st.push(i);                                                                             
-            }
+            // }
         }
         return ans;
     }
@@ -20,14 +20,14 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         for(int i = n-1; i >= 0; i--) {
-            if(st.isEmpty()) {
-                st.push(i);
-                ans[i] = n-1;
-            } else {
+            // if(st.isEmpty()) {
+            //     st.push(i);
+            //     ans[i] = n-1;
+            // } else {
                 while(!st.isEmpty() && nums[st.peek()] >= nums[i]) st.pop();
                 ans[i] = st.size() > 0 ? st.peek()-1 : n-1;
                 st.push(i);
-            }
+            // }
         }
         return ans;
     }
@@ -76,19 +76,14 @@ class Solution {
         // }
         // return ans;
 
-        // method 3:
+        // method 3: using stack
 
         int[] leftS = leftSmaller(heights);
-        // for(int i : leftS) System.out.print(i+" ");
-        // System.out.println();
         int[] rightS = rightSmaller(heights);
-        // for(int i : rightS) System.out.print(i+" ");
-        // System.out.println();
 
         // int ans = 0;
         for(int i = 0; i < n; i++) {
             int t = (rightS[i] - leftS[i]+1) * heights[i];
-            // System.out.print(t+" ");
             ans = Math.max(ans, t);
         }
         return ans;
