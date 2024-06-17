85b7d522ba9994bb0bcf01657770d3c2f30e5368
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-06-17 09:46:27 +0530

    Time: 4 ms (56.86%) | Memory: 39.8 MB (97.71%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,22 +1,30 @@
 class Solution {
-    // int sqrt(int x) {
-    //     if(x == 0 || x == 1) return x;
-    //     long st = 1, end = x/2;
-    //     long ans = 0;
-    //     while(st <= end) {
-    //         long mid = st + (end - st)/2;
-    //         long y = mid*mid;
-    //         if(y==x) return (int)mid;
-    //         else if(y>x) end = mid-1;
-    //         else {
-    //             st = mid+1;
-    //             ans = mid;
-    //         }
-    //     }
-    //     return (int)ans;
-    // }
+    int sqrt(int x) {
+        if(x == 0 || x == 1) return x;
+        long st = 1, end = x/2;
+        long ans = 0;
+        while(st <= end) {
+            long mid = st + (end - st)/2;
+            long y = mid*mid;
+            if(y==x) return (int)mid;
+            else if(y>x) end = mid-1;
+            else {
+                st = mid+1;
+                ans = mid;
+            }
+        }
+        return (int)ans;
+    }
     public boolean judgeSquareSum(int n) {
-        int c = (int)Math.sqrt(n);
+        // int c = (int)Math.sqrt(n);
+        // for(int b = 0; b <= c; b++) {
+        //     int a = (int)Math.sqrt(n-b*b);
+        //     if((a*a + b*b) == n) 
+        //         return true;
+        // }
+
+        // method 2:
+        int c = sqrt(n);
         for(int b = 0; b <= c; b++) {
             int a = (int)Math.sqrt(n-b*b);
             if((a*a + b*b) == n) 
