41c4e7b3b49f54147d2c15aa8061090b7bc700f3
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-07-16 09:12:52 +0530

    Time: 26 ms (57.68%) | Memory: 60.3 MB (61.70%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -3,63 +3,69 @@ class Solution {
         int m = 0, p = 0, g = 0;
 
         int n = garbage.length;
-        boolean first = false;
+        boolean m_first = false, p_first = false, g_first = false;
 
-        // for m
         for(int i = n-1; i >= 0; i--) {
             String s = garbage[i];
 
-            int cnt = 0;
             for(int j = 0; j < s.length(); j++) {
-                if(s.charAt(j) == 'M') {
-                    first = true;
-                    cnt++;
+                char ch = s.charAt(j);
+                if(ch == 'M') {
+                    m_first = true;
+                    m++;
+                } else if(ch == 'P') {
+                    p_first = true;
+                    p++;
+                } else {
+                    g_first = true;
+                    g++;
                 }
             }
 
-            m += cnt;
-            if(first && i-1 >= 0) {
-                m += travel[i-1];
+            if(i-1 >= 0) {
+                if(m_first) m += travel[i-1];
+                if(p_first) p += travel[i-1];
+                if(g_first) g += travel[i-1];
             }
         }
 
         // for p
-        first = false;
-        for(int i = n-1; i >= 0; i--) {
-            String s = garbage[i];
+        // first = false;
+        // for(int i = n-1; i >= 0; i--) {
+        //     String s = garbage[i];
 
-            int cnt = 0;
-            for(int j = 0; j < s.length(); j++) {
-                if(s.charAt(j) == 'P') {
-                    first = true;
-                    cnt++;
-                }
-            }
+        //     int cnt = 0;
+        //     for(int j = 0; j < s.length(); j++) {
+        //         if(s.charAt(j) == 'P') {
+        //             first = true;
+        //             cnt++;
+        //         }
+        //     }
 
-            p += cnt;
-            if(first && i-1 >= 0) {
-                p += travel[i-1];
-            }
-        }
+        //     p += cnt;
+        //     if(first && i-1 >= 0) {
+        //         p += travel[i-1];
+        //     }
+        // }
 
-        // for g
-        first = false;
-        for(int i = n-1; i >= 0; i--) {
-            String s = garbage[i];
+        // // for g
+        // first = false;
+        // for(int i = n-1; i >= 0; i--) {
+        //     String s = garbage[i];
 
-            int cnt = 0;
-            for(int j = 0; j < s.length(); j++) {
-                if(s.charAt(j) == 'G') {
-                    first = true;
-                    cnt++;
-                }
-            }
+        //     int cnt = 0;
+        //     for(int j = 0; j < s.length(); j++) {
+        //         if(s.charAt(j) == 'G') {
+        //             first = true;
+        //             cnt++;
+        //         }
+        //     }
 
-            g += cnt;
-            if(first && i-1 >= 0) {
-                g += travel[i-1];
-            }
-        }
+        //     g += cnt;
+        //     if(first && i-1 >= 0) {
+        //         g += travel[i-1];
+        //     }
+        // }
 
         return m+p+g;
     }
