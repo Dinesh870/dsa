4c4e9acffccf397cdf903f273c2a2c8a20db788e
Dinesh870 <112678835+Dinesh870@users.noreply.github.com>
2024-07-16 09:16:15 +0530

    Time: 26 ms (57.68%) | Memory: 60.1 MB (66.40%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -1,6 +1,5 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        // int m = 0, p = 0, g = 0;
         int ans = 0;
 
         int n = garbage.length;
@@ -11,19 +10,9 @@ class Solution {
 
             for(int j = 0; j < s.length(); j++) {
                 char ch = s.charAt(j);
-                if(ch == 'M') {
-                    m_first = true;
-                    // m++;
-                    // ans++;
-                } else if(ch == 'P') {
-                    p_first = true;
-                    // p++;
-                    // ans++;
-                } else {
-                    g_first = true;
-                    // g++;
-                    // ans++;
-                }
+                if(ch == 'M') m_first = true;
+                else if(ch == 'P') p_first = true;
+                else g_first = true;
                 ans++;
             }
 
@@ -34,45 +23,6 @@ class Solution {
             }
         }
 
-        // for p
-        // first = false;
-        // for(int i = n-1; i >= 0; i--) {
-        //     String s = garbage[i];
-
-        //     int cnt = 0;
-        //     for(int j = 0; j < s.length(); j++) {
-        //         if(s.charAt(j) == 'P') {
-        //             first = true;
-        //             cnt++;
-        //         }
-        //     }
-
-        //     p += cnt;
-        //     if(first && i-1 >= 0) {
-        //         p += travel[i-1];
-        //     }
-        // }
-
-        // // for g
-        // first = false;
-        // for(int i = n-1; i >= 0; i--) {
-        //     String s = garbage[i];
-
-        //     int cnt = 0;
-        //     for(int j = 0; j < s.length(); j++) {
-        //         if(s.charAt(j) == 'G') {
-        //             first = true;
-        //             cnt++;
-        //         }
-        //     }
-
-        //     g += cnt;
-        //     if(first && i-1 >= 0) {
-        //         g += travel[i-1];
-        //     }
-        // }
-
-        // return m+p+g;
         return ans;
     }
 }
